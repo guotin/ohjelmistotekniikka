416cd656d7bbb7ff0c1857d56e7373896e7a0d1a
guotin <eerokauhanen@outlook.com>
2019-03-18 19:14:50 +0200

    viikko2 edistymistä

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,44 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void saldoAlussaOikein() {
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    
+    @Test
+    public void lisaysToimii() {
+        kortti.lataaRahaa(50);
+        kortti.lataaRahaa(-150);
+        assertEquals(1050, kortti.saldo());
+    }
+    
+    @Test
+    public void rahanOttoVahentaaRahaa() {
+        kortti.otaRahaa(50);
+        assertEquals(950, kortti.saldo());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosOtetaanLiikaa() {
+        kortti.otaRahaa(10000);
+        assertEquals(1000, kortti.saldo());
+    }
+    
+    @Test
+    public void palauttaaTrueJosRahatRiitti() {
+        assertEquals(true, kortti.otaRahaa(7));
+    }
+    @Test
+    public void palauttaaFalseJosRahatEiRiittanyt() {
+        assertEquals(false, kortti.otaRahaa(7000));
     }
 }
